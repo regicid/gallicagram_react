c590de29893fae2a4a145a02d90b05f716898dfd
Benoit De Courson <b.de-courson@LSE-Mac-b.de-courson-G4VQDR7RV6>
2026-02-10 15:14:13 +0000

    analytics

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import './i18n.js';
+import { Analytics } from "@vercel/analytics/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
